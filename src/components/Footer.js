fa5b826ba98c061e7a29d69d9ad338cccbb35103
nhokphal <phalnhok@gmail.com>
2020-08-28 23:17:33 +0700

    3 files modified, 3 added, 1 deleted

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import react from "react"
+
+function Footer () {
+    return(
+        <div>
+            <h5>Footer</h5>
+        </div>
+    );
+  }
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css"
 
-import App from "./App";
+ 
+function App(){
+  return (
+    <div>
+        <h1>Nhok phal</h1>
+        <p>This is my first react!</p>
+          <ul>
+          <li>1 My first task is to create react site</li>
+          <li>2 Then i will create my own applicaton </li>
+          <li>3 and deploy a business for my business in the future</li>
+          </ul>
+        <nav>
+          </nav>
+            <main>This is where my items go..
+            </main>   
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById("root"))
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
